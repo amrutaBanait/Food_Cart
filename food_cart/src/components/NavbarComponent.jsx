fd7f4d08aec7c4f8b90782990952844088459de7
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-01-05 13:32:42 +0530

    nav compo

--- food_cart/src/components/NavbarComponent.jsx
+++ food_cart/src/components/NavbarComponent.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
-import {useState} from "react";
-import {AiOutlineMenu} from "react-icons/ai";
-
-
-function NavbarComponent () {
-  const [nav, setNav] = useState(false)
+import React, { useState } from "react";
+import {
+  AiFillTag,
+  AiOutlineClose,
+  AiOutlineMenu,
+  AiOutlineSearch,
+} from "react-icons/ai";
+import { BsFillCartFill } from "react-icons/bs";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdFavorite, MdHelp } from "react-icons/md";
+import { FiTrendingUp } from "react-icons/fi";
+import { FaUserFriends, FaWallet } from "react-icons/fa";
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
   return (
-    <div>
-{/* left side of navigation bar */}
-<div>
-  <div>
-    <AiOutlineMenu size={30} />
-  </div>
-</div>
+    <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
+      {/* Left Side  */}
+      <div className="flex items-center">
+        <div onClick={() => setNav(!nav)} className="cursor-pointer">
+          <AiOutlineMenu size={30} />
+        </div>
+        <h1 className="text-2xl  sm:text-3xl lg:text-4xl px-2">
+      EveryDay <span className="font-semibold">Hits</span>
+        </h1>
+        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+          <p className="bg-black text-white rounded-full p-2">Delivery</p>
+          <p className="p-2">Pickup</p>
+        </div>
+      </div>
+      
+      
+      
+    
     </div>
-  )
-}
+  );
+};
 
-
-export default NavbarComponent ;
+export default Navbar;
