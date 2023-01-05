4e8bc1e8000dbebc735247c14f47f5aa3b3c9799
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-01-05 21:03:26 +0530

    side dwawer menu added

--- food_cart/src/components/NavbarComponent.jsx
+++ food_cart/src/components/NavbarComponent.jsx
@@ -48,7 +48,32 @@ const Navbar = () => {
         <BsFillCartFill size={20} className="mr-2" /> Cart
       </button>
 
-      
+      {/* Mobile Menu  */}
+      {/* { overlay} */}
+
+      {nav ? (
+        <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      ) : (
+        ""
+      )}
+      {/* Side drawer Menu  */}
+      <div
+        className={
+          nav
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+        }
+      >
+        <AiOutlineClose
+         onClick={() => setNav(!nav)}
+          size={30}
+          className="absolute right-4 top-4 cursor-pointer"
+        />
+        <h2 className="text-2xl p-4">
+          Slick <span className="font-semibold">Eats</span>
+        </h2>
+        
+      </div>
     </div>
   );
 };
