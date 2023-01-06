96e4ffc212f8b95d3e0f4b9534931d8fedc1b902
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-01-06 19:57:41 +0530

    mobile menu

--- food_cart/src/components/Navbar.jsx
+++ food_cart/src/components/Navbar.jsx
@@ -3,8 +3,10 @@ import {
  
   AiOutlineMenu,
   AiOutlineSearch,
+  
 } from "react-icons/ai";
 import { BsFillCartFill } from "react-icons/bs";
+
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -16,15 +18,14 @@ const Navbar = () => {
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl  sm:text-3xl lg:text-4xl px-2">
-          Slick <span className="font-semibold">Eats</span>
+          Spicy <span className="font-semibold">Eats</span>
         </h1>
         <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
           <p className="bg-black text-white rounded-full p-2">Delivery</p>
           <p className="p-2">Pickup</p>
         </div>
       </div>
-      Search Input 
-
+      
       <div
         className="bg-gray-200 rounded-full flex items-center px-2 w-[200px]
           sm:w-[400px] lg:w-[500px]"
@@ -36,13 +37,23 @@ const Navbar = () => {
           placeholder="Search for foods, restaurants, cuisines"
         />
       </div>
-      {/* Cart button  */}
-      <button
+     {/* Cart button  */}
+     <button
         className="bg-black text-white 
       hidden md:flex items-center py-2 rounded-full"
       >
         <BsFillCartFill size={20} className="mr-2" /> Cart
       </button>
+{/* Mobile Menu  */}
+      {/* { overlay} */}
+
+      {nav ? (
+        <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      ) : (
+        ""
+      )}
+     
+    
 
       
   
