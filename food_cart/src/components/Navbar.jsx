1cb7be3c32d93b16ea3edda64f39dd2dbaa922ed
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-01-06 20:07:46 +0530

    added side drawer

--- food_cart/src/components/Navbar.jsx
+++ food_cart/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {
  
   AiOutlineMenu,
   AiOutlineSearch,
-  
+  AiOutlineClose
 } from "react-icons/ai";
 import { BsFillCartFill } from "react-icons/bs";
 
@@ -52,6 +52,24 @@ const Navbar = () => {
       ) : (
         ""
       )}
+       {/* Side drawer Menu  */}
+       <div
+        className={
+          nav
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+        }
+      >
+        <AiOutlineClose
+         onClick={() => setNav(!nav)}
+          size={30}
+          className="absolute right-4 top-4 cursor-pointer"
+        />
+        <h2 className="text-2xl p-4">
+          Slick <span className="font-semibold">Eats</span>
+        </h2>
+        
+      </div>
      
     
 
