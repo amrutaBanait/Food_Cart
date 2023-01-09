3fabfebf244304e268c9d2297dcc7dbed09b3b16
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-01-09 23:23:10 +0530

    filter by category

--- food_cart/src/components/Food.jsx
+++ food_cart/src/components/Food.jsx
@@ -2,11 +2,29 @@ import React from "react";
 import  {useState} from "react";
 import {data} from "../Data/data";
 
-function Food(){
+const Food = () => {
+    //console.log(data);
+
     const [foods, setFoods] = useState(data)
-    return (
-        <div>Food</div>
+    
+    
+  //   Filter Type burgers/pizza/etc
+
+    const filterType = (category) => {
+        setFoods(
+            data.filter((item) => {
+                return item.category === category
+            })
+        )
+    }
+    return(
+        <div>
+            <h1>
+        Top Rated Menu Items
+      </h1>
+        </div>
     )
 }
 
 export default Food;
+
