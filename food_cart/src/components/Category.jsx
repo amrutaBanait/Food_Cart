960fd2f483fdabcf993e3499745c7bcd5d4899e4
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-01-09 22:56:57 +0530

    category styled

--- food_cart/src/components/Category.jsx
+++ food_cart/src/components/Category.jsx
@@ -9,20 +9,25 @@ function Category(){
          </h1>        
          {/*Mapping of Items*/}
 
-       <div>
+       <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">
         {categories.map((item) => {
           return (
             <div
               key={item.id}
+              className="bg-gray-100 cursor-pointer p-4 flex  items-center 
+              border shadow-lg justify-between rounded-lg  hover:scale-105 duration-300"
             >
-              <h2>{item.name}</h2>
+              <h2 className="font-bold sm:text-xl">{item.name}</h2>
               <img
+                className="w-20 h-20 object-cover"
                 src={item.image}
                 alt={item.name}
               />
             </div>
-          );
-        })}
+          )
+        }
+        )
+        }
       </div>
         </div>
 
