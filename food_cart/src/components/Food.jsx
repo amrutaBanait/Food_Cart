eb8b5a29f440e4110ee85f5836f6ac96c72bebe4
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-01-10 16:36:09 +0530

    styled

--- food_cart/src/components/Food.jsx
+++ food_cart/src/components/Food.jsx
@@ -116,10 +116,10 @@ const Food = () => {
               alt={item.name}
               className='w-full h-[200px] object-cover rounded-t-lg'
             />
-            <div >
-              <p>{item.name}</p>
+            <div className='flex justify-between px-2 py-4'>
+              <p className='font-bold'>{item.name}</p>
               <p>
-                <span>
+                <span className='bg-green-500 text-white p-1 rounded-full'>
                   {item.price}
                 </span>
               </p>
